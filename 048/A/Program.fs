e67010e57783ac5c9c3382667e098d03fa0db21a
numnum <kazurego7@gmail.com>
2019-09-19 21:03:50 +0900

    ABC048 A-B

---
+++ 048/A/Program.fs
@@ -0,0 +1,8 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    let [ _; s; _ ] = Console.ReadLine().Split(' ') |> Array.toList
+    let ans = "A" + (string s.[0]) + "C"
+    printfn "%s" ans |> ignore
+    0 // return an integer exit code
---
+++ 048/B/Program.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.org
+open System
+
+[<EntryPoint>]
+let main argv =
+    let [ a; b; x ] =
+        Console.ReadLine().Split(' ')
+        |> List.ofArray
+        |> List.map int64
+
+    let ans =
+        if a = 0L then b / x + 1L
+        else b / x - (a - 1L) / x
+
+    printfn "%d" ans
+    0 // return an integer exit code
